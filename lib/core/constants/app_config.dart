f7ff2c69a80a78c4a13abeaf6eb0b2996ae3e564
jaehyuk <jaehyuk.dev@gmail.com>
2025-07-13 08:15:45 +0900

    [FIX] end point 분기 수정

--- lib/core/constants/app_config.dart
+++ lib/core/constants/app_config.dart
@@ -8,15 +8,15 @@ class AppConfig {
   // API Base URL
   static String get apiBaseUrl {
     return isDevelopment
-        ? 'http://localhost:8080/api'
-        : 'https://api.reatypartners.store/api';
+        ? 'https://api.reatypartners.store/api'
+        : 'http://localhost:8080/api';
+
   }
 
   // 이미지/파일 Base URL
   static String get fileBaseUrl {
     return isDevelopment
-        ? 'http://localhost:8080/static'
-        // : 'https://api.xn--h50bw7n3vhqjd47vujaqg.site';
-        : 'https://api.reatypartners.store/';
+        ? 'https://api.reatypartners.store/'
+        : 'http://localhost:8080/';
   }
 }
