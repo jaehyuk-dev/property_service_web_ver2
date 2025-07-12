80de8047979ce404932c2e32e3fbdccc69ea0033
jaehyuk <jaehyuk.dev@gmail.com>
2025-07-13 08:23:37 +0900

    [FIX] end point 분기 재수정

--- lib/core/config/app_config.dart
+++ lib/core/config/app_config.dart
@@ -4,17 +4,32 @@ class AppConfig {
   static bool get isDevelopment => kDebugMode;
   static bool get isProduction => !kDebugMode;
 
+  // // API Base URL
+  // static String get apiBaseUrl {
+  //   return isDevelopment
+  //       ? 'http://localhost:8080/api'
+  //       : 'https://api.xn--h50bw7n3vhqjd47vujaqg.site/api';
+  // }
+  //
+  // // 이미지/파일 Base URL
+  // static String get fileBaseUrl {
+  //   return isDevelopment
+  //       ? 'http://localhost:8080'  // ✅ /static 제거 - 백엔드에서 직접 /building/, /property/ 경로 제공
+  //       : 'https://api.xn--h50bw7n3vhqjd47vujaqg.site';
+  // }
+
   // API Base URL
   static String get apiBaseUrl {
     return isDevelopment
-        ? 'http://localhost:8080/api'
-        : 'https://api.xn--h50bw7n3vhqjd47vujaqg.site/api';
+        ? 'https://api.reatypartners.store/api'
+        : 'http://localhost:8080/api';
+
   }
 
   // 이미지/파일 Base URL
   static String get fileBaseUrl {
     return isDevelopment
-        ? 'http://localhost:8080'  // ✅ /static 제거 - 백엔드에서 직접 /building/, /property/ 경로 제공
-        : 'https://api.xn--h50bw7n3vhqjd47vujaqg.site';
+        ? 'https://api.reatypartners.store/'
+        : 'http://localhost:8080/';
   }
 }
